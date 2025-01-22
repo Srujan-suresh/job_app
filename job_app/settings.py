cd2068e56f0df880055a55e5c807a2201b1341c3
Srujan Suresh <srujansuresh28@gmail.com>
2025-01-22 16:43:57 +0530

    updates setting

--- job_app/settings.py
+++ job_app/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-3=w=+_o(1j5(a*v4kifz8sg$vm-228*678bg4xm#y+$lik#jyz
 DEBUG = True
 
  # Add allowed hosts if needed
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','job_app.com','*']
 
 # Application definition
 INSTALLED_APPS = [
